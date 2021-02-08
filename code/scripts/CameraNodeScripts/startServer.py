f60aba6b8aabd1eebdb33519812b4fc05e75ec78
jj <>
2021-02-08 21:31:05 +0100

    Listen for button click to make photo

--- code/scripts/CameraNodeScripts/startServer.py
+++ code/scripts/CameraNodeScripts/startServer.py
@@ -9,13 +9,20 @@ from scripts.DataManager import current_ms_time
 
 deviceName = "RPI Zero + Camera"
 
+photoButtonPin = 17 # Move to common place with LEDS pins
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(photoButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 LedChanger.initLedChanger()
 
 app = FlaskAPI(__name__)
 
 lastAlertTime = 0
 
+def photoButtonEvent(channel):
+    if GPIO.input(photoButtonPin) == GPIO.HIGH:
+        CameraManager.makePhoto()
 
 @app.route('/alertPhoto', methods=["POST"])
 def alertPhoto():
@@ -57,3 +64,4 @@ startTime = current_ms_time()
 DataManager.refreshServerAddressFromFile()
 HeartbeatManager.initHeartbeatThread()
 NgrokAddressesManager.sendAddressesToServerUntilSuccess()
+GPIO.add_event_detect(photoButtonPin, GPIO.BOTH, callback=photoButtonEvent)
