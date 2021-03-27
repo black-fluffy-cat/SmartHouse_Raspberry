1ee692b8f14536d5d8c278b419180cad526e9b82
jj <>
2021-03-27 12:47:36 +0100

    Rearrange files

---
+++ code/CameraNode/startCameraNode.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export FLASK_APP=/home/pi/Desktop/smarthouse_projects/SmartHouse_Raspberry/code/CameraNode/src/startServer.py
+flask run --host=0.0.0.0
--- code/scripts/CameraNodeScripts/startFlaskServer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/home/pi/Desktop/smarthouse_projects/SmartHouse_Raspberry/code/scripts/CameraNodeScripts/startFlaskApp.sh /home/pi/Desktop/smarthouse_projects/SmartHouse_Raspberry/code/scripts/CameraNodeScripts/startServer.py
