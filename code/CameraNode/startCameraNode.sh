617a2c91e3dafc256148198b6c69cf9522af1e35
jj <>
2021-03-27 20:58:21 +0100

    Edit startCameraNode script

--- code/CameraNode/startCameraNode.sh
+++ code/CameraNode/startCameraNode.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+cd /home/pi/Desktop/smarthouse_projects/SmartHouse_Raspberry/code/CameraNode
 export FLASK_APP=/home/pi/Desktop/smarthouse_projects/SmartHouse_Raspberry/code/CameraNode/src/startServer.py
 flask run --host=0.0.0.0
